59111010b85057576585672a93f2ce9e4f5abbfb
Jake <jake.lgendreau@gmail.com>
2024-01-04 19:52:50 -0800

    Reads width and height

--- ascii.cpp
+++ ascii.cpp
@@ -3,67 +3,48 @@
 
 using namespace std;
 
-struct BMPSig{
-    unsigned char data[2];
-    BMPSig() {data[0] = data[1] = 0;}
-};
+unsigned char* readBMP(char* filename, int &width, int &height){
 
-struct BMPHeader{
-    unsigned int fileSize;
-    unsigned short reserved1;
-    unsigned short reserved2;
-    unsigned int dataOffset;
-    BMPHeader() : fileSize(0), reserved1(0), reserved2(0), dataOffset(0) {}
-};
+    //open file
+    FILE* f = fopen(filename, "rb");
+    unsigned char info[54];
 
-struct pixel{
-    unsigned int red;
-    unsigned int green;
-    unsigned int blue;
-    pixel() : red(0), green(0), blue(0) {}
-};
+    //Read the 54-byte header
+    fread(info, sizeof(unsigned char), 54, f);
 
-void readHeader(ifstream &fin, BMPSig &sig, BMPHeader &header){
-    if(!fin)
-        return;
+    //extract image height and width from header
+    width = *(int*)&info[18];
+    height = *(int*)&info[22];
 
-    //Go to start of file
-    fin.seekg(0, ios::beg);
+    //allocate 3 bytes per pixel
+    int size = 3 * width * height;
+    unsigned char* data = new unsigned char[size];
 
-    //Read the signature + header
-    fin.read((char*) &sig, sizeof(sig));
-    fin.read((char*) &header, sizeof(header));
-}
+    //read the rest of the data at once
+    fread(data, sizeof(unsigned char), size, f);
+    fclose(f);
 
-void printHeader(BMPSig sig, BMPHeader header){
-    cout << "========BMP HEADER========" << endl;
-    cout << "+ Signature : " << sig.data[0] << sig.data[1] << endl;
-    cout << "+ File size : " << header.fileSize << " bytes" << endl;
-    cout << "+ Reserved1 : " << header.reserved1 << endl;
-    cout << "+ Reserved2 : " << header.reserved2 << endl;
-    cout << "+ Data Offset : " << header.dataOffset << " bytes" << endl;
+    //flip the order of every 3 bytes
+    for(int i = 0; i < size; i++){
+        unsigned char tmp = data[i];
+        data[i] = data[i+2];
+        data[i+2] = tmp;
+    }
+
+    return data;
 }
 
 int main(int argc, char* argv[]){
-    //Get command line arg
+    //check args
     if(argc != 2){
-        cout << "Improper usage! ./ascii.out <bmp file name>" << endl;
+        cout << "Invalid usage! ./ascii.cpp <bmp file name>" << endl;
         return -1;
     }
 
-    //Read in file
-    ifstream fin(argv[1], ios::binary);
+    int height;
+    int width;
 
-    //Define a sig and header
-    BMPSig sig;
-    BMPHeader header;
+    unsigned char* data = readBMP(argv[1], width, height);
 
-    //print empty header
-    printHeader(sig, header);
-    //read bmp header
-    readHeader(fin, sig, header);
-    //print populated header
-    printHeader(sig, header);
-
-    return 0;
+    cout << "Width: " << width << "\nHeight: " << height << endl;
 }
